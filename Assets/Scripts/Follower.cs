ce1b7079322e0cfdc5088cf8b8f8625e5fa3ca74
Guido Tello <lpmaleinn@gmail.com>
2022-04-28 14:42:45 -0300

    Updated Player Movement
    With Q & E, side movement.
    With A & D, lateral movement.
    With S & W, frontal movement.
    With space moves in up vector direction.

--- Assets/Scripts/Follower.cs
+++ Assets/Scripts/Follower.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class Follower : MonoBehaviour
 {
     [SerializeField] private Transform target;
+    [SerializeField] private float distanceFromObject;
 
     private void Update()
     {
         transform.LookAt(target);
+        Vector3 auxPos = target.position - transform.position;
+        auxPos.Normalize();
+        auxPos *= 1/distanceFromObject;
+        transform.position = auxPos;
     }
 
 }
--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -6,24 +6,73 @@ public class Player_Movement : MonoBehaviour
 {
     [SerializeField] private float horizontalSpeed;
     [SerializeField] private float sideRotationSpeed;
+    [SerializeField] private float frontalRotationSpeed;
+    [SerializeField] private float jumpSpeed;
+
+    private bool sideRotationInput = false;
+    private bool frontalRotationInput = false;
+    private bool horizontalInput = false;
 
     private Rigidbody rb;
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(0, -horizontalSpeed * Time.deltaTime, 0); //Sin fuerzas, el avion deja de girar cuando se suelta el input
+            horizontalInput = true;
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(0, horizontalSpeed * Time.deltaTime, 0);
+            horizontalInput = true;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, 0, -sideRotationSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, 0, sideRotationSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Rotate(frontalRotationSpeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Rotate(-frontalRotationSpeed * Time.deltaTime, 0, 0);
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(transform.up * jumpSpeed);
+        }
+    }
+
+
+    /*
     private void FixedUpdate()
     {
         if (Input.GetKey(KeyCode.Q))
-            rb.AddTorque(new Vector3(0, -horizontalSpeed * Time.deltaTime, 0));
+            rb.AddTorque(new Vector3(0, -horizontalSpeed * Time.deltaTime, 0)); //Esto es con fuerzas. El avion sigue girando por mas que no haya input
         if (Input.GetKey(KeyCode.E))
             rb.AddTorque(new Vector3(0, horizontalSpeed * Time.deltaTime, 0));
-        /*
+        
         if (Input.GetKey(KeyCode.D))
             rb.AddTorque(new Vector3(0, 0, -sideRotationSpeed * Time.deltaTime)); 
         if (Input.GetKey(KeyCode.A))
             rb.AddTorque(new Vector3(0, 0, sideRotationSpeed * Time.deltaTime));
-        */
     }
-
+    */
 }
